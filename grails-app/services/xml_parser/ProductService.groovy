9a0f8c918d017f7e8a65bfc683a3e4b8ea956388
d.v.hozyashev <olaf.hd@gmail.com>
2017-07-24 18:00:45 +0500

    multithreading

--- grails-app/services/xml_parser/ProductService.groovy
+++ grails-app/services/xml_parser/ProductService.groovy
@@ -6,6 +6,14 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.xml.sax.SAXParseException
 
+import java.util.concurrent.ArrayBlockingQueue
+import java.util.concurrent.BlockingQueue
+import java.util.concurrent.Callable
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import java.util.concurrent.Future
+import java.util.concurrent.FutureTask
+
 /**
  * Сервисный уровень главной таблицы
  */
@@ -15,6 +23,8 @@ class ProductService {
     def categoryService
 
     private static final Logger logger = LoggerFactory.getLogger(ProductService.class);
+
+    private static final ExecutorService consumers = Executors.newFixedThreadPool(2);
 
     /**
      * Парсинг xml файла
@@ -42,46 +52,118 @@ class ProductService {
     private def importXmlToDB(def xmlContent) {
         logger.debug("importXmlToDB()")
 
-        def productId
-        def countSuccessful = 0, countError = 0
-        logger.debug("Запуск парсинга XML")
-        xmlContent.products.product.each { product ->
-            try {
-                logger.debug("Импортируется сущность с наименованием: ${product.title[0].text()}")
+        BlockingQueue<Product> queue = new ArrayBlockingQueue<Product>(50)
 
-                //Если текущее поле product_id пустое или не существует - берем предыдущее известное значение
-                productId = product.product_id[0].text() ?: productId
+        FutureTask<String> futureProducer = new FutureTask<>(new Producer(queue, xmlContent))
+        Thread producer = new Thread(futureProducer)
+        //Future consumer = consumers.submit(new Consumer(queue))
+        //Thread consumer = new Thread(new Consumer(queue))!!!! TODO: static
 
-                //Если поле inet_price отсутствует или пустое - обнуляем поле price сущности, иначе - берем значение поля price
-                def price = product.inet_price[0]?.text()?.length() > 0 ? product.price[0]?.text() ?: 0 : 0
+        producer.start()
+        //consumer.start()
 
-                def rating = Float.valueOf(product.rating[0]?.text()) ?: Float.valueOf("0")
+        String message
+        try {
+            message = futureProducer.get()
+        } catch (InterruptedException e) {
+            message = "Ошибка получения результата парсинга"
+        }
+        logger.debug(message)
 
-                def entity = new Product(
-                        productId: productId,
-                        title: product.title[0]?.text(),
-                        description: product.description[0]?.text(),
-                        rating: rating,
-                        price: price,
-                        image: product.image[0]?.text()
-                )
+        return message
+    }
 
-                //расчитываем категорию товара
-                def category = categoryService.getByRating(rating);
+    class Producer implements Callable<String> {
+        BlockingQueue<Product> queue
+        def xmlContent
 
-                entity.category = category
-                logger.debug("Результирующая сущность product: ${entity}, category: ${entity.category}")
-                entity.save(flush: true, failOnError: true);
+        Producer(BlockingQueue<Product> queue, def xmlContent) {
+            this.queue = queue
+            this.xmlContent = xmlContent
+        }
 
-                countSuccessful++
-            } catch (NumberFormatException | ValidationException e) {
-                logger.error("Ошибка парсинга записи product: ${e.getMessage()}")
-                countError++
+        @Override
+        String call() {
+            String threadName = "${Thread.currentThread().getName()}"
+            logger.debug("${threadName}: запущен поток для пасинга файла и генерации объектов Product")
+
+            def productId
+            def countSuccessful = 0, countError = 0
+            logger.debug("${threadName}: Запуск парсинга XML")
+            xmlContent.products.product.each { product ->
+                try {
+                    logger.debug("${threadName}: Импортируется сущность с наименованием: ${product.title[0].text()}")
+
+                    //Если текущее поле product_id пустое или не существует - берем предыдущее известное значение
+                    productId = product.product_id[0].text() ?: productId
+
+                    //Если поле inet_price отсутствует или пустое - обнуляем поле price сущности, иначе - берем значение поля price
+                    def price = product.inet_price[0]?.text()?.length() > 0 ? product.price[0]?.text() ?: 0 : 0
+
+                    def rating = Float.valueOf(product.rating[0]?.text()) ?: Float.valueOf("0")
+
+                    //расчитываем категорию товара
+                    def category = categoryService.getByRating(rating);
+
+                    def entity = new Product(
+                            productId: productId,
+                            title: product.title[0]?.text(),
+                            description: product.description[0]?.text(),
+                            rating: rating,
+                            price: price,
+                            image: product.image[0]?.text(),
+                            category: category
+                    )
+
+                    logger.debug("${threadName}: Результирующая сущность product: ${entity}, category: ${entity.category}")
+
+                    queue.put(entity)
+
+                    countSuccessful++
+                } catch (NumberFormatException | ValidationException e) {
+                    logger.error("${threadName}: Ошибка парсинга записи product: ${e.getMessage()}")
+                    countError++
+                }
+            }
+
+            logger.debug("${threadName}: поток парсинга завершил свою работу.")
+
+            String text = "Импорт завершен! Импортировано ${countSuccessful} объектов. Ошибок при импорте: ${countError}"
+
+            return text
+        }
+    }
+
+    class Consumer implements Runnable {
+        BlockingQueue<Product> queue
+
+        Consumer(BlockingQueue<Product> queue) {
+            this.queue = queue
+        }
+
+        @Override
+        void run() {
+            String threadName = "${Thread.currentThread().getName()}"
+
+            logger.debug("${threadName}: запущен поток для сохранения данных в БД.")
+            while (true) {
+                try {
+                    Thread.sleep(1)
+
+                    Product product = queue.poll()
+
+                    if (product != null) {
+                        saveEntity(product)
+
+                        logger.debug("${threadName}: Объект сохранен в БД")
+                    }
+
+                } catch (InterruptedException e) {
+                    logger.debug("${threadName}: принудительная остановка потока.")
+                    break
+                }
             }
         }
-        logger.debug("Импорт завершен! Импортировано ${countSuccessful} объектов. Ошибок при импорте: ${countError}")
-
-        return "Импорт завершен! Импортировано ${countSuccessful} объектов. Ошибок при импорте: ${countError}"
     }
 
     /**
@@ -122,13 +204,13 @@ class ProductService {
         product.category = categoryService.getByRating(product.rating)
     }
 
-    def parsingInputStream(InputStream inputStream, BufferedWriter logWriter){
+    def parsingInputStream(InputStream inputStream, BufferedWriter logWriter) {
         String message
 
         try {
             def startTime = System.currentTimeMillis()
 
-            manualLogging(logWriter,"==============");
+            manualLogging(logWriter, "==============");
             manualLogging(logWriter, "${new Date(startTime)}: Запуск автоматической процедуры импорта данных.")
 
             def xmlContent = getXmlContent(inputStream)
@@ -162,7 +244,17 @@ class ProductService {
         return message
     }
 
-    private void manualLogging(BufferedWriter logWriter, String message){
+    private void manualLogging(BufferedWriter logWriter, String message) {
         if (logWriter != null) logWriter.writeLine(message)
+    }
+
+    /**
+     * Сохранение записи вынесено в отдельную функцию, т.к. автомаппинг спринга ломает сигнатуры наследуемых методов потоков...
+     * @param product Сохраняемая сущность
+     * @return void
+     */
+    @Transactional
+    def saveEntity(Product product) {
+        product.save(flush: true, failOnError: true);
     }
 }
